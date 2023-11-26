c524acf02d56c91e2c5e6b52f31acdf27859f75f
Muhammad Ramdhan Fitra Hidayat <fitrahidayaat@gmail.com>
2023-11-27 06:51:40 +0700

    fix some ui

--- lib/src/constants/api_consts.dart
+++ lib/src/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
